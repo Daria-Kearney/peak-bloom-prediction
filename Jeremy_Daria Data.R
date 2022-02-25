394e9095c789627b3ab7c4f68fa2180950fac7fa
Daria Kearney <dkearne2@gmu.edu>
2022-02-24 19:13:37 -0500

    changing the warm and cold buckets, adding glm performance, and making both files the same

--- Jeremy_Daria Data.R
+++ Jeremy_Daria Data.R
@@ -50,15 +50,15 @@ tempall$cold<- ifelse(tempall$tavg <= -115, 1, 0)
 #Creating binary variable label "warm" for when average temperature is <= 100(1/10 °C)=> 50°F
 tempall$hot<- ifelse(tempall$tavg >= 100, 1, 0)
 
-#Getting the data for just Nov, Dec, Jan, Feb, March, & April
-temp_all_hot_cold<-subset(tempall, format(date, "%m") %in% c("01", "02", "03", "04", "11", "12"))
+#Getting the data for just Nov, Dec, Jan, & Feb
+temp_all_hot_cold<-subset(tempall, format(date, "%m") %in% c("01", "02", "11", "12"))
 
 #Creating new variable of the sum of days that meet the requirement above & 
 #grouping them in Spring & Winter so I can aggregate the date
 temp_all_hot_cold<- temp_all_hot_cold %>%
   mutate(month = as.integer(format(date, "%m"))) %>%
   mutate(year = as.integer(format(date, "%Y"))) %>%
-  mutate(seasons= ifelse(month %in% c(2, 3, 4), "Spring", "Winter")) %>%
+  mutate(seasons= ifelse(month < 11, "Spring", "Winter")) %>%
   mutate(year= ifelse(month >= 11, year+1, year )) %>%
   group_by(year, seasons, location) %>%
   mutate(cold= sum(cold, na.rm = TRUE)) %>%
@@ -86,6 +86,56 @@ str(temp_all_hot_cold)
 
 #Aggregating data so that it is by location and have just one row for each year for all locations
 temp_all_hot_cold= aggregate(. ~ year + location, data = temp_all_hot_cold, FUN= sum)
+
+#Observing general multiple linear regression model
+#Merging bloom days for the three locations note: takes away Vancouver days
+cherry <- read.csv("data/washingtondc.csv") %>% 
+  bind_rows(read.csv("data/liestal.csv")) %>% 
+  bind_rows(read.csv("data/kyoto.csv"))
+
+cherrytembin<- merge(cherry, temp_all_hot_cold, by= c("location", "year"))
+
+#Getting rid of unnecessary variables
+cherrytembin<- select(cherrytembin, -c("alt", "lat", "long", "bloom_date"))
+
+#Fitting linear regression to see how it performs
+test_ls_fit<- lm(bloom_doy ~ year*location + cold + hot, data = cherrytembin)
+summary(test_ls_fit)
+
+#RSS
+sum(resid(test_ls_fit)^2)
+deviance(test_ls_fit)
+#MSE
+mean(test_ls_fit$residuals^2)
+#AIC
+AIC(test_ls_fit)
+#All better than standard LR given in demo
+
+#Fitting it for the past predictions for 3 locations
+cherrytembin<- cherrytembin %>% 
+  bind_cols(predicted_doy = predict(test_ls_fit, newdata= cherrytembin))
+
+# Plot the predictions alongside the actual observations for 1950 up to 2020.
+cherrytembin %>% 
+  ggplot(aes(x = year, y = predicted_doy)) +
+  geom_line() +
+  geom_point(aes(y = bloom_doy)) +
+  facet_grid(cols = vars(str_to_title(location))) +
+  labs(x = "Year", y = "Peak bloom (days since Jan 1st)")
+
+#Calculating Absolute Difference for three locations between 1950-2020 getting the sums for 2011-2020
+diff<- cherrytembin %>%
+  group_by(year, location) %>%
+  mutate(absdiff = abs(predicted_doy-bloom_doy)) %>%
+  #filter(year>= 2011) %>% #
+  group_by(year) %>%
+  filter(year>=2011) %>%
+  summarise(sum= sum(absdiff))
+print(diff)
+#Calculating the mean of the sum of the Absolute Difference for three locations between 1950-2020
+#The mean over the 70 years is 15.5 which performs better than demo
+mean(diff$sum)
+sum(diff$sum)
 
 ############################################################################################################
 ############################################################################################################
--- Peak Bloom Day Prediction.R
+++ Peak Bloom Day Prediction.R
@@ -1,9 +1,6 @@
-####################################################################################################################
-####################################################################################################################
-####################################################################################################################
-####################################################################################################################
-####################################################################################################################
-####################################################################################################################
+############################################################################################################
+############################################################################################################
+################################################################################################################################################
 #Daria's Data Set
 #Creating data set for Cold and Warm Buckets- Daria
 library(dplyr)
@@ -17,7 +14,7 @@ get_tmax<- function (stationid) {
                date_min = "1950-01-01", date_max = "2020-12-31")[[1]]}%>%
   mutate(year = as.integer(format(date, "%Y"))) %>%
   group_by(date) %>%
-  summarize(tmax)
+  summarise(tmax)
 
 #Get all four location for 1950-2020
 days_max<-
@@ -32,7 +29,7 @@ get_tmin<- function (stationid) {
                date_min = "1950-01-01", date_max = "2020-12-31")[[1]]}%>%
   mutate(year = as.integer(format(date, "%Y"))) %>%
   group_by(date) %>%
-  summarize(tmin) 
+  summarise(tmin) 
 
 #Get all four location for 1950-2020
 days_min<-
@@ -62,6 +59,7 @@ temp_all_hot_cold<- temp_all_hot_cold %>%
   mutate(month = as.integer(format(date, "%m"))) %>%
   mutate(year = as.integer(format(date, "%Y"))) %>%
   mutate(seasons= ifelse(month < 11, "Spring", "Winter")) %>%
+  mutate(year= ifelse(month >= 11, year+1, year )) %>%
   group_by(year, seasons, location) %>%
   mutate(cold= sum(cold, na.rm = TRUE)) %>%
   mutate(hot= sum(hot, na.rm = TRUE)) %>%
@@ -89,9 +87,7 @@ str(temp_all_hot_cold)
 #Aggregating data so that it is by location and have just one row for each year for all locations
 temp_all_hot_cold= aggregate(. ~ year + location, data = temp_all_hot_cold, FUN= sum)
 
-#Might have to change the criteria for the average temperature.
-#Still seeing what is the best model and predictions
-
+#Observing general multiple linear regression model
 #Merging bloom days for the three locations note: takes away Vancouver days
 cherry <- read.csv("data/washingtondc.csv") %>% 
   bind_rows(read.csv("data/liestal.csv")) %>% 
@@ -103,7 +99,7 @@ cherrytembin<- merge(cherry, temp_all_hot_cold, by= c("location", "year"))
 cherrytembin<- select(cherrytembin, -c("alt", "lat", "long", "bloom_date"))
 
 #Fitting linear regression to see how it performs
-test_ls_fit<- lm(bloom_doy ~ year + location + cold + hot, data = cherrytembin)
+test_ls_fit<- lm(bloom_doy ~ year*location + cold + hot, data = cherrytembin)
 summary(test_ls_fit)
 
 #RSS
@@ -140,9 +136,73 @@ print(diff)
 #The mean over the 70 years is 15.5 which performs better than demo
 mean(diff$sum)
 sum(diff$sum)
-####################################################################################################################
-####################################################################################################################
-####################################################################################################################
-####################################################################################################################
-####################################################################################################################
-####################################################################################################################
+
+############################################################################################################
+############################################################################################################
+############################################################################################################
+##Jeremy's Data/ R Code
+# Load data
+cherry <- read.csv("data/washingtondc.csv") %>% 
+  bind_rows(read.csv("data/liestal.csv")) %>% 
+  bind_rows(read.csv("data/kyoto.csv"))
+
+get_climatedata <- function (stationid) {
+  historic_climatedata <- ghcnd_search(stationid = stationid, var = c("TMAX", "TMIN", "PRCP", "SNWD"), 
+                                       date_min = "1850-01-01", date_max = "2023-01-31")
+  
+  multiFull <- merge(merge(merge(
+    historic_climatedata[[1L]],
+    historic_climatedata[[2L]][-c(4:6)], all=TRUE, by=c("id", "date")),
+    historic_climatedata[[3L]][-c(4:6)], all=TRUE, by=c("id", "date")),
+    historic_climatedata[[4L]][-c(4:6)], all=TRUE, by=c("id", "date"))
+  
+  multiFull %>% mutate(year = as.integer(format(date, "%Y")),
+                       month = as.integer(strftime(date, '%m')) %% 12, # make December "0"
+                       season = cut(month, breaks = c(0, 2, 5, 8, 11),
+                                    include.lowest = TRUE,
+                                    labels = c("Winter", "Spring", "Summer", "Fall")),
+                       year = if_else(month == 0, year + 1L, year)) %>%
+    group_by(year, month) %>%
+    summarize(tmax_avg = mean(tmax, na.rm = TRUE),
+              tmin_avg = mean(tmin, na.rm = TRUE),
+              prcp_avg = mean(prcp, na.rm = TRUE),
+              prcp_tot = sum(prcp, na.rm = TRUE),
+              snwd_avg = mean(snwd, na.rm = TRUE),
+              snwd_tot = sum(snwd, na.rm = TRUE))
+}
+
+#climate_dc <- tibble(location = "washingtondc", get_climatedata("USC00186350"))
+#climate_dc_month <- tibble(location = "washingtondc", get_climatedata("USC00186350"))
+
+historic_data <-
+  tibble(location = "washingtondc", get_climatedata("USC00186350")) %>%
+  bind_rows(tibble(location = "liestal", get_climatedata("GME00127786"))) %>%
+  bind_rows(tibble(location = "kyoto", get_climatedata("JA000047759"))) %>%
+  bind_rows(tibble(location = "vancouver", get_climatedata("CA001108395")))
+
+historic_data <- historic_data[historic_data$month %in% c(0:4),] # subset for Dec-Apr data
+historic_data <- historic_data %>% 
+  pivot_wider(names_from = month, values_from = c(tmax_avg, tmin_avg, prcp_avg, prcp_tot, snwd_avg, snwd_tot))
+historic_data <- historic_data %>% full_join(cherry, by = c("location", "year"))
+
+# Location       Years of RNOAA data   Years for Cherry bloom_doy
+# Kyoto          1951-2022             812-2021
+# Liestal        1953-2022             1894-2021
+# Washington DC  1948-2021             1921-2021
+# Vancouver      1957-2022             N/A
+sunlight <- read.csv(file.choose())
+
+
+sunlight <- sunlight %>% mutate(month = ifelse(Month.Code==12, 0, Month.Code),
+                                year = ifelse(Month.Code==12, year_raw+1, year_raw)) %>%
+  group_by(location, year, month) %>%
+  summarise(sunlight_avg = mean(sunlight_avg, na.rm=TRUE)) %>%
+  filter(month %in% c(0:4)) %>%
+  pivot_wider(names_from = month, values_from = sunlight_avg, names_prefix = 'sunlight_avg_')
+
+df_final <- historic_data %>% full_join(sunlight, by = c("location", "year")) %>%
+  full_join(cherry, by=c("location", "year"))
+
+df_final %>% group_by(location) %>% summarise(initial_year = min(year), final_year = max(year))
+
+djdata<- full_join(df_final, temp_all_hot_cold, by= c("year", "location")) 
