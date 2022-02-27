d789d604088cfc8b5cbb40ee557c38d3a50547fa
Daria Kearney <dkearne2@gmu.edu>
2022-02-27 02:49:19 -0500

    added test_model 1.R

--- Peak Bloom Day Prediction.R
+++ Peak Bloom Day Prediction.R
@@ -10,6 +10,7 @@ library(randomForest)
 library(missForest) # missForest()
 library(gbm) # gbm()
 library(xgboost) # xgb.train() and xgboost()
+library(Hmisc) #prcomp()
 
 # Load data
 cherry <- read.csv("data/washingtondc.csv") %>% 
@@ -201,6 +202,49 @@ str(temp_all_hot_cold)
 #Aggregating data so that it is by location and have just one row for each year for all locations
 temp_all_hot_cold= aggregate(. ~ year + location, data = temp_all_hot_cold, FUN= sum)
 
+#######################
+# Load EPA & NPN Data #
+#######################
+
+phenomtrics <- read.csv("data/USA-NPN_individual_phenometrics_data.csv")
+intensity <- read.csv("data/USA-NPN_status_intensity_observations_data.csv")
+phenom_dict <- read.csv("data/USA-NPN_individual_phenometrics_datafield_descriptions.csv")
+int_dict <- read.csv("data/USA-NPN_status_intensity_datafield_descriptions.csv")
+EPA <- read.csv("data/combined_EPA_dataset.csv")
+
+#########################
+#   Data Manipulation   # 
+#########################
+
+# Find the minimum year of available data, bind to cherry dataset by that year 
+min(EPA$year)
+data <- left_join(dplyr::filter(cherry, year >= 1990)
+                  ,EPA,by="year")
+
+# Impute missing values in EPA dataset with average values
+data$Carbon.dioxide <- impute(data$Carbon.dioxide, mean)
+data$Methane <- impute(data$Methane,mean)
+data$Nitrous.oxide <- impute(data$Nitrous.oxide,mean)
+data$HFCs..PFCs..SF6..and.NF3 <- impute(data$HFCs..PFCs..SF6..and.NF3,mean)
+data$Anomaly <- impute(data$Anomaly,mean)
+data$Winter <- impute(data$Winter,mean)
+data$Spring <- impute(data$Spring,mean)
+data$Summer <- impute(data$Summer,mean)
+data$Fall <- impute(data$Fall,mean)
+data$Earth.s.surface <- impute(data$Earth.s.surface,mean)
+data$Lower.troposphere..measured.by.satellite...UAH. <- impute(
+  data$Lower.troposphere..measured.by.satellite...UAH., mean
+)
+data$Lower.troposphere..measured.by.satellite...RSS. <- impute(
+  data$Lower.troposphere..measured.by.satellite...RSS., mean
+)
+
+# Renaming the last two to be better naming conventions
+names(data)[18:19] <- c("UAH","RSS")
+
+# Removing data that isn't relevant
+data1 <- select(data,!c("lat","long","bloom_date"))
+
 ############################
 # Plots and visualizations #
 ############################
@@ -241,14 +285,6 @@ df_final %>%
   facet_grid(cols = vars(str_to_title(location))) +
   labs(x = "TMAX Avg in Feb", y = "Peak bloom (days since Jan 1st)")
 
-# Plot the predictions alongside the actual observations for 1950 up to 2020.
-cherrytembin %>% 
-  ggplot(aes(x = year, y = predicted_doy)) +
-  geom_line() +
-  geom_point(aes(y = bloom_doy)) +
-  facet_grid(cols = vars(str_to_title(location))) +
-  labs(x = "Year", y = "Peak bloom (days since Jan 1st)")
-
 #Look at all years for different locations and seasons
 temp_exp %>% filter() %>%
   ggplot(aes(x= tavg)) + 
@@ -269,6 +305,77 @@ temp_exp %>%
   stat_qq()+
   stat_qq_line()+
   facet_wrap(~location)
+
+########################################################
+# EPA Data Exploration- Principal Components Analysis  #
+########################################################
+
+dc <- data1 %>% filter(location=="washingtondc")
+
+# Removing altitude since it is zero and can't be scaled
+dc1 <- select(dc,-c(location,alt))
+
+#Principal Components Analysis for Washingtondc
+pr.dc <- prcomp(dc1,scale=TRUE)
+pr.dc$rotation
+biplot(pr.dc,scale=0)
+
+# Note, in the biplot of the first two principal components, the variables
+# Nitrous.oxide, bloom day, and anomaly are clustered together. Seasons are grouped
+#   by the season that follow each other (spring-winter, fall-summer)
+
+kyoto <- data1 %>% filter(location=="kyoto")
+
+# Removing altitude since it is all the same and non-numeric column
+kyoto1 <- select(kyoto,-c(location,alt))
+
+#Principal Components Analysis for Kyoto
+pr.kyoto <- prcomp(kyoto1,scale=TRUE)
+pr.kyoto$rotation
+biplot(pr.kyoto,scale=0)
+pr.var.kyoto <- pr.kyoto$sdev^2
+pr.var.kyoto
+pve.kyoto <- pr.var/sum(pr.var.kyoto)
+pve.kyoto
+
+# Similar factpattern as the DC results
+
+liestal <- data1 %>% filter(location=="liestal")
+
+liestal1 <- select(liestal,-c(location,alt))
+
+#Principal Components Analysis for Kyoto
+pr.liestal <- prcomp(liestal1,scale=TRUE)
+pr.liestal$rotation
+par(mfrow=c(1,3))
+biplot(pr.dc,scale=0)
+biplot(pr.kyoto,scale=0)
+biplot(pr.liestal,scale=0)
+
+#################### Linear regression ############################
+#Using uncorrelated variables: nitrous oxide, methane,carbon dioxide
+ls_fit.epa <- lm(bloom_doy ~ Carbon.dioxide + Methane + Nitrous.oxide +
+                   location * year, data = data1)
+summary(ls_fit.epa)
+
+predict(ls_fit.epa)
+
+predictions.epa <- data1 %>% 
+  bind_cols(predicted_doy = predict(ls_fit.epa, newdata = .))
+
+par(mfrow=c(1,1))
+
+######### Scatter plot of predicted bloom day verus actual bloom day ###########
+# Simple graph to see if high predicted values occur with high actual values
+
+predictions.epa %>% 
+  ggplot(aes(x = bloom_doy, y = predicted_doy,
+             color=location)) +
+  geom_point() +
+  labs(x = "Bloom Day Since Jan 1st", y = "Predicted Peak Day Since Jan 1st")
+
+# Predictive error
+mean(abs(predictions.epa$predicted_doy - predictions.epa$bloom_doy)/predictions.epa$bloom_doy)*100
 
 ########################################################################
 # General Multiple Linear Regression Model for Hot and Cold Covariates #
@@ -312,6 +419,14 @@ print(diff)
 #The mean over the 70 years is 15.5 which performs better than demo
 mean(diff$sum)
 sum(diff$sum)
+
+# Plot the predictions alongside the actual observations for 1950 up to 2020.
+cherrytembin %>% 
+  ggplot(aes(x = year, y = predicted_doy)) +
+  geom_line() +
+  geom_point(aes(y = bloom_doy)) +
+  facet_grid(cols = vars(str_to_title(location))) +
+  labs(x = "Year", y = "Peak bloom (days since Jan 1st)")
 
 ###################
 # OLS model: Year #
@@ -916,8 +1031,10 @@ Predict<- Predict %>%
   group_by(location, year) %>% summarize_all(list(~toString(unique(.)))) %>%
   transform(cold= as.numeric(cold), hot= as.numeric(hot), tavg= as.numeric(tavg))
 
-#Full Data set with forecasting 
+##################### Full Data Set with Forecasting ###########################
+
 djdata<- full_join(df_final, Predict, by= c("year", "location")) 
+
 ################################################################################
 ################################################################################
 ################################################################################
